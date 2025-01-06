9fb1cecda3a7d28aeec06b87140164a528bd2ed4
annalatyeva <anysha_s@mail.ru>
2025-01-06 22:29:11 +0500

    feat: finish stack

--- src/stack.js
+++ src/stack.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require("../extensions/index.js");
 
 /**
  * Implement the Stack with a given interface via array.
@@ -13,22 +13,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	push(element) {
+		if (!this.stack) {
+			this.stack = [];
+		}
+		this.stack.push(element);
+	}
 
-  pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	pop() {
+		if (this.stack.length === 0 || !this.stack) {
+			return undefined;
+		}
+		return this.stack.pop();
+	}
 
-  peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	peek() {
+		if (this.stack.length === 0 || !this.stack) {
+			return undefined;
+		}
+		return this.stack[this.stack.length - 1];
+	}
 }
 
 module.exports = {
-  Stack
+	Stack,
 };
